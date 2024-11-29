70177bc92243b3a3d454c396d9d1b007d0b52393
Marc <marc.blattmann@digitecgalaxus.ch>
2024-11-29 15:58:10 +0100

    created

---
+++ app/app/protected/account-settings/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import * as React from 'react'
+
+export default function AccountSettings() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Account Settings</h1>
+      {/* Add other settings components or content here */}
+    </div>
+  )
+}
